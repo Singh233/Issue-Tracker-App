eb49262642b59938a737f55056b6ff114f38079f
Sanambir Singh <sanambir123@gmail.com>
2023-03-23 19:56:56 +0530

    Change color of the active navigation links

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -56,3 +56,33 @@ if (path.includes('projects') || path.includes('new-project') || path.includes('
     navbar.classList.remove('bg');
 }
 
+if (path.includes('issues')) {
+    // add the active class to the navbar links
+    const issues = document.querySelector('#a-issues-page');
+    issues.classList.add('active');
+    // jquery to remove the active class from the other links
+    $('#a-home-page').removeClass('active');
+    $('#a-new-project-page').removeClass('active');
+
+
+
+} else if (path.includes('search')) {
+    $('#a-home-page').removeClass('active');
+    $('#a-new-project-page').removeClass('active');
+    $('#a-issues-page').removeClass('active');
+} else if (path.includes('projects')) {
+    // add the active class to the navbar links
+    const issues = document.querySelector('#a-issues-page');
+    issues.classList.add('active');
+    // jquery to remove the active class from the other links
+    $('#a-home-page').removeClass('active');
+    $('#a-new-project-page').removeClass('active');
+
+} else if (path.includes('new-project')) {
+    const newProject = document.querySelector('#a-new-project-page');
+    newProject.classList.add('active');
+
+    $('#a-issues-page').removeClass('active');
+    $('#a-home-page').removeClass('active');
+} 
+
